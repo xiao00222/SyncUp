25d7ceaf6e89bd97befa4ca0dbc6676a2b00b864
xiao00222 <acfarhan123@gmail.com>
2025-08-01 20:52:26 +0500

    reactsteup

--- API/Program.cs
+++ API/Program.cs
@@ -10,10 +10,12 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+.WithOrigins("http://localhost:5173", "https://localhost:5173"));
 app.MapControllers();
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
