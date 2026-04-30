caabcc28eb19a11e99798f3e892619f1c87f54a2
xiao00222 <acfarhan123@gmail.com>
2026-04-30 18:59:55 +0500

    vite fix

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   build:{
-    outDir:"../API/wwwroot",
+    outDir: process.env.VERCEL ? 'dist' : '../API/wwwroot',
     chunkSizeWarningLimit:1700,
     emptyOutDir:true
   },
